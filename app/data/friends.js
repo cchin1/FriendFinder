2b46895d0c7897a19c8f12ba563f6900be0359ab
Candice Chin <chincandice2007@gmail.com>
2019-10-23 09:59:46 -0700

    corrected friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,54 +5,54 @@
 
 var friends = [
     {
-    name: "Ahmed",
-    photo: "",
-    scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    name: "Andrew",
+    photo: "https://i.pinimg.com/564x/a0/6d/29/a06d29ed80225eb3975ac3c20821c00a.jpg",
+    scores:[1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     },
     {
-    name: "Ahmed",
-    photo: "",
-    scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    name: "Bart",
+    photo: "https://i.pinimg.com/564x/a0/4d/79/a04d792f68a0079b3356523e5548ca28.jpg",
+    scores:[2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
     },
     {
-    name: "Ahmed",
-    photo: "",
-    scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    name: "Carl",
+    photo: "https://i.pinimg.com/originals/f0/c5/5e/f0c55e93aad04a8d09ba4198a67a5c67.jpg",
+    scores:[3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
     },
     {
-    name: "Ahmed",
-    photo: "",
-    scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    name: "Dave",
+    photo: "https://i.pinimg.com/564x/df/88/8b/df888b52e65454de230ef0e14ff0b36e.jpg",
+    scores:[4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
     },
     {
-    name: "Ahmed",
-    photo: "",
-    scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    name: "Elton",
+    photo: "https://i.pinimg.com/564x/08/c7/95/08c79577c9eab0abb81b4d1c57708aa0.jpg",
+    scores:[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     },
     {
-    name: "Ahmed",
-    photo: "",
-    scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    name: "Frankie",
+    photo: "https://i.pinimg.com/564x/6d/62/f1/6d62f143fa673dd5118773ba09ce8e90.jpg",
+    scores:[1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
     },
     {
-    name: "Ahmed",
-    photo: "",
-    scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    name: "George",
+    photo: "https://i.pinimg.com/originals/e8/b6/b5/e8b6b51f06e0ee4c38bdd07432007fb0.jpg",
+    scores:[2, 3, 4, 5, 1, 2, 3, 4, 5, 1]
     },
     {
-    name: "Ahmed",
-    photo: "",
-    scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    name: "Harry",
+    photo: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+    scores:[3, 4, 5, 1, 2, 3, 4, 5, 1, 2]
     },
     {
-    name: "Ahmed",
-    photo: "",
-    scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    name: "Ian",
+    photo: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    scores:[4, 5, 1, 2, 3, 4, 5, 1, 2, 3]
     },
     {
-    name: "Ahmed",
-    photo: "",
-    scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    name: "Jake",
+    photo: "https://format-com-cld-res.cloudinary.com/image/private/s--Wx2cvfOJ--/c_limit,g_center,h_700,w_65535/a_auto,fl_keep_iptc.progressive,q_95/v1/9b7bdf033345c9c308f660dfc53a026f/20160110-IMG_7614_web-crop2.jpg",
+    scores:[5, 1, 2, 3, 4, 5, 1, 2, 3, 4]
     },
 ];
 
