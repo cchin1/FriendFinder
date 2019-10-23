25663cbf4a2082f9373e0f834004bb4629a991ea
Candice Chin <chincandice2007@gmail.com>
2019-10-23 09:41:35 -0700

    kinda works

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -25,6 +25,6 @@ module.exports = function(app) {
 
 // If no matching route is found default to home
   app.use(function(req, res) {
-    res.sendFile(path.join(_dirname + "/../public/home.html"));
+    res.sendFile(path.join(__dirname + "/../public/home.html"));
   });
 };
