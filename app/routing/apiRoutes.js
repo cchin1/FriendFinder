8ca8b01caec2aff7b64c84beee86f4232bef0917
Candice Chin <chincandice2007@gmail.com>
2019-10-23 14:23:29 -0700

    made it worse

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -35,13 +35,7 @@ module.exports = function(app) {
   // Add a new friend entry
   app.post("/api/friends", function(req, res) {
 
-    // Computing best match
-    var bestMatch = {
-        name: "",
-        photo: "",
-        friendDifference: 1000
-    }
-
+    
     console.log(req.body);
 
     // Here we take the result of the user's survey POST and parse it.
@@ -50,10 +44,10 @@ module.exports = function(app) {
 
     var userScores = userData.scores;
     console.log("userScores = " + userScores);
-
-    console.log(userScores);
-
-    // This variable will calculate the difference between the user's scores and the scores of each user in the database
+    
+    // Computing best match
+    var matchName = "";
+    var matchImage = "";
     var totalDifference = 10000;
 
     // Here we loop through all the friend possibilties in the database. This is the beginning of a nested for-loop.
@@ -64,18 +58,18 @@ module.exports = function(app) {
         var diff = 0;
 
         // We then loop through all the scores of each friend
-        for (var j = 0; j < friends[i].scores[j]; j++) {
+        for (var j = 0; j < userScores.length; j++) {
 
             // We calculate the difference between the scores and sum them into the totalDifference
             diff += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
 
             // If the sum of differences is less than the differences of the current "best match"
-            if (diff <= bestMatch.friendDifference) {
+            if (diff <= totalDifference) {
 
                 // Reset the bestMatch to be the new friend.
-                bestMatch.name = friends[i].name;
-                bestMatch.photo = friends[i].photo;
-                bestMatch.friendDifference = diff;
+                totalDifference = diff;
+				matchName = friends[i].name;
+				matchImage = friends[i].photo;
             }
         }
     }
@@ -85,7 +79,6 @@ module.exports = function(app) {
     friends.push(userData);
 
     // Return a JSON with the user's bestMatch. This will be used by the HTML in the next page
-    res.json({status: "Great!", bestMatch: bestMatch});
+    res.json({status: "Great!", matchName: matchName, matchImage: matchImage});
   });
-
 }
